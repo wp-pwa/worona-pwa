05a6a8f597d5ef81660705f528d0407bbace92d7
Luis Herranz <luisherranz@gmail.com>
2017-08-04 13:19:59 +0200

    Add logger to server and only in dev mode.

--- core/store.js
+++ core/store.js
@@ -1,26 +1,42 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
-import logger from 'redux-logger';
+import { createLogger } from 'redux-logger';
 
 let store = null;
 
+// Add Redux Dev Tools.
 const composeDevTools =
   (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const composeEnhancers = composeWithDevTools({ serialize: false });
 
-const composeEnhancers = composeWithDevTools({
-  serialize: false,
-});
-
+// Init saga and create middlewares.
 const sagaMiddleware = createSagaMiddleware();
-
-const clientMiddleware = [sagaMiddleware, logger];
+const clientMiddleware = [sagaMiddleware];
 const serverMiddleware = [sagaMiddleware];
 
+// Add logger in dev mode.
+if (process.env.NODE_ENV !== 'production') {
+  const { createLogger } = require('redux-logger');
+  clientMiddleware.push(createLogger());
+  serverMiddleware.push(createLogger({
+    titleFormatter: ({ type }) => `\n\naction: ${type}\n`,
+    colors: {
+      title: false,
+      prevState: false,
+      action: false,
+      nextState: false,
+      error: false,
+    },
+  }));
+}
+
 export const initStore = ({ reducer, initialState = {} }) => {
+  // Create store for the server.
   if (typeof window === 'undefined') {
     return createStore(reducer, initialState, compose(applyMiddleware(...serverMiddleware)));
   } else {
+    // Create store for the client, only if it hasn't been created before.
     if (!store) {
       store = createStore(
         reducer,
