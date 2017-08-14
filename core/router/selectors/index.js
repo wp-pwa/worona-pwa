d9be00b9c9d4edab2f54f4cd28acabd37ca57bc8
Luis Herranz <luisherranz@gmail.com>
2017-08-14 17:50:56 +0200

    Use WP query naming.

--- core/router/selectors/index.js
+++ core/router/selectors/index.js
@@ -8,14 +8,14 @@ export const getType = createSelector(
   getQuery,
   query => {
     const { p, cat, tag, author, page_id, s, attachment_id } = query;
-    if (p) return 'post';
-    else if (cat) return 'category';
+    if (p) return 'p';
+    else if (cat) return 'cat';
     else if (tag) return 'tag';
     else if (author) return 'author';
     else if (page_id) return 'page_id';
     else if (s) return 's';
     else if (attachment_id) return 'attachment_id';
-    else return 'home';
+    else return 'latest_posts';
     }
 );
 
