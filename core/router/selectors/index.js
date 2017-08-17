3409e7341326c2fb8b1ac0f58e63bc65daa52ee8
Luis Herranz <luisherranz@gmail.com>
2017-08-17 16:02:19 +0200

    Go back to old url types

--- core/router/selectors/index.js
+++ core/router/selectors/index.js
@@ -7,29 +7,29 @@ export const getPathname = state => state.router.pathname;
 export const getType = createSelector(
   getQuery,
   query => {
-    const { p, cat, tag, author, page_id, s, attachment_id } = query;
-    if (p) return 'p';
-    else if (cat) return 'cat';
+    const { p, page_id, cat, tag, author, s, attachment_id } = query;
+    if (p) return 'post';
+    else if (page_id) return 'page';
+    else if (cat) return 'category';
     else if (tag) return 'tag';
     else if (author) return 'author';
-    else if (page_id) return 'page_id';
-    else if (s) return 's';
-    else if (attachment_id) return 'attachment_id';
-    else return 'latest_posts';
-    }
+    else if (s) return 'search';
+    else if (attachment_id) return 'media';
+    else return 'latest';
+  }
 );
 
 export const getId = createSelector(
   getQuery,
   query => {
-    const { p, cat, tag, author, page_id, s, attachment_id } = query;
+    const { p, page_id, cat, tag, author, s, attachment_id } = query;
     if (p) return parseInt(p);
+    else if (page_id) return parseInt(page_id);
     else if (cat) return parseInt(cat);
     else if (tag) return parseInt(tag);
     else if (author) return parseInt(author);
-    else if (page_id) return parseInt(page_id);
     else if (s) return parseInt(s);
     else if (attachment_id) return parseInt(attachment_id);
     else return 0;
-    }
+  }
 );
