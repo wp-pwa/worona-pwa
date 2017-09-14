bae141b927edbf46abc2adeb3d2e2df0b604fc27
Eduardo Campaña <asrielo@gmail.com>
2017-09-14 18:42:50 +0200

    Fixes #32 PWA button

--- static/go-back-to-worona.js
+++ static/go-back-to-worona.js
@@ -5,8 +5,10 @@
     window.scrollTo(0, 0);
     var button = document.createElement('div');
     button.id = 'back-to-pwa';
-    button.style =
-      'width: 55px; height: 55px; position: fixed; bottom: 20px; right: 20px; background-color: rgb(61, 124, 223); border-radius: 50%; box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 10px, rgba(0, 0, 0, 0.23) 0px 3px 10px; display: flex; justify-content: center; align-items: center; z-index: 5000;';
+    button.setAttribute(
+      'style',
+      'width: 55px; height: 55px; position: fixed; bottom: 20px; right: 20px; background-color: rgb(61, 124, 223); border-radius: 50%; box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 10px, rgba(0, 0, 0, 0.23) 0px 3px 10px; display: flex; justify-content: center; align-items: center; z-index: 5000;'
+    );
 
     var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     svg.setAttribute('viewBox', '0 0 100 100');
