21ad22184d14cf4db1b59abc4a3c299b63a3fc90
Eduardo Campaña <asrielo@gmail.com>
2017-11-03 11:03:58 +0100

    Adds getAmp selector

--- core/build/selectors/index.js
+++ core/build/selectors/index.js
@@ -3,6 +3,7 @@ import { createSelector } from 'reselect';
 export const getExtensions = state => state.build.extensions;
 export const getTheme = state => state.build.theme;
 export const getSsr = state => state.build.ssr;
+export const getAmp = state => state.build.amp;
 
 export const getPackages = createSelector(getExtensions, getTheme, (extensions, theme) => [
   ...Object.values(extensions),
