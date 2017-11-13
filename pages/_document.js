95a239e4275fd2616652c610d7efa45207dde7b5
Eduardo Campaña <asrielo@gmail.com>
2017-11-13 18:46:41 +0100

    Added amp-ad

--- pages/_document.js
+++ pages/_document.js
@@ -40,10 +40,9 @@ export default class MyDocument extends Document {
     return (
       <html amp="">
         <Head>
-          <meta charSet="utf-8" />
           <meta
             name="viewport"
-            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"
           />
           <meta name="HandheldFriendly" content="true" />
           <style amp-boilerplate="">{`body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}`}</style>
@@ -81,6 +80,7 @@ export default class MyDocument extends Document {
             custom-element="amp-instagram"
             src="https://cdn.ampproject.org/v0/amp-instagram-0.1.js"
           />
+          <script async custom-element="amp-ad" src="https://cdn.ampproject.org/v0/amp-ad-0.1.js" />
           {styleTags}
         </Head>
         <body>
